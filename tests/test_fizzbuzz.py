cb415b9fc975bc1c05dadd435c58bd4813edd448
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-20 11:47:51 +0000

    whitespace refactored

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -3,10 +3,10 @@ from src import fizzbuzz
 import pytest
 
 def test_fizz():
-  assert(fizzbuzz.fizzbuzz(3)) == "fizz" 
+    assert(fizzbuzz.fizzbuzz(3)) == "fizz" 
 
 def test_buzz(): 
-  assert(fizzbuzz.fizzbuzz(5)) == "buzz"
+    assert(fizzbuzz.fizzbuzz(5)) == "buzz"
 
 def test_fizzbuzz():
-  assert(fizzbuzz.fizzbuzz(15)) == "fizzbuzz"
+    assert(fizzbuzz.fizzbuzz(15)) == "fizzbuzz"
